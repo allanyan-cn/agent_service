8506b3f4d1bf3582cae4e07dc8fb6952d75b57dd
Allan Yan <allanyan.cn@gmail.com>
2026-01-15 23:08:34 +0800

    Add log, tool and validator(work in Reflection mode)

--- agents/weather_agent/agent.py
+++ agents/weather_agent/agent.py
@@ -1,7 +1,8 @@
 import os
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from pydantic_ai import Agent, ModelSettings
 from pydantic_ai.models.openai import OpenAIChatModel
+import logging
 
 os.environ['OPENAI_API_KEY'] = 'N/A'
 os.environ['OPENAI_BASE_URL'] = 'http://localhost:1234/v1'
@@ -15,22 +16,47 @@ class WeatherResult(BaseModel):
     weather: str
     temperature_c: float
 
+    @field_validator('weather')
+    @classmethod
+    def check_weather(cls, v):
+        if v not in ["sunny", "cloudy", "overcast", "rain", "storm", "snow", "fog"]:
+            logging.error("weather is unknown")
+            raise ValueError("weather is unknown, weather should be in 'sunny', 'cloudy', 'overcast', 'rain', 'storm', 'snow', 'fog'")
+        return v
+    
+    @field_validator('temperature_c')
+    @classmethod
+    def check_temperature(cls, v):
+        if v >= 50 or v <= -50:
+            raise ValueError("temperature is abnornal")
+        return v
+
 
 local_model = OpenAIChatModel(
     model_name="gpt-oss-20b",
     provider="openai",
     settings=ModelSettings(
         seed=42,
-        temperature=0.0
+        temperature=0.0,
+        timeout=300,
     ),
 )
+
+def get_weather(req: WeatherRequest) -> WeatherResult:
+    logging.info(f"Call get_weather, city: {req.city}")
+    response = WeatherResult(city=req.city, weather="sunn", temperature_c=25.5)
+    logging.info(f"Return is {response}")
+    return response
 
 weather_agent = Agent[WeatherResult](
     model=local_model,
     system_prompt=(
         "You are a weather inquiry assistant."
-        "Based on the city provided by the user, return the current weather conditions and temperature in Celsius."
+        "Based on the city provided by the user, return the current weather conditions and temperature in Celsius via tools"
         "If the information cannot be determined, please state that clearly."
-        "Please return the response strictly in JSON format."
+        "Please automatically correct spelling errors in output."
     ),
+    tools=[get_weather],
+    output_type=WeatherResult,
+    retries=3
 )
--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import asyncio
 from pathlib import Path
 from runtime.registry import AgentRegistry
 from runtime.builder import build_agent_service
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(),          # console
+    ]
+)
 
 async def main():
     # registry = AgentRegistry()
@@ -9,8 +17,12 @@ async def main():
     # registry.register(service)
 
     service = build_agent_service("agents/weather_agent")
-    result = await service.call("How is the weather today?")
-    print(result)
+    try:
+        result = await service.call("What will the weather be like in Paris tomorrow?")
+        print(result)
+    except (Exception) as e:
+        # 达到重试上限或发生其他错误时，返回你定义的特定信息
+        print(f"The agent tried its best, but still failed. Error details:{e}")
 
 if __name__ == "__main__":
     asyncio.run(main())
